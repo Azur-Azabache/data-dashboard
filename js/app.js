5d3d23ae08e46d8df73174dbdee66bf18ef51481
MariacristinaOrtiz <mari_d17@hotmail.com>
2017-11-30 12:31:47 -0500

    Añade graficos terminados(archivo graphic.js , app.js)

--- js/app.js
+++ js/app.js
@@ -26,29 +26,54 @@ window.addEventListener('load', function(event) {
     case event.target.value === 'aqp2016II':
       /* Al entrar a la generación 'aqp2016II' se remueve la clase "display-none" a section que contiene la informacion deseada de esa generación*/
       section.classList.remove('display-none');
-      /* Llamando a la funcion de la GRAFICO ENROLLMENT (ver graphics.js)*/
+      /* Llamando a la funcion de la GRAFICO ENROLLMENT (ver graphics.js, line:3)"aqp2016II"*/
       enrollmentGraphic(10, 124);
-      /* Llamando a la funcion de la GRAFICO ACHIEVEMENT (ver graphics.js)*/
+      /* Llamando a la funcion de la GRAFICO ACHIEVEMENT (ver graphics.js, line:4)"aqp2016II"*/
       achievementGraphic(15, 50);
-      /* Llamando a la funcion de la GRAFICO NET PROMOTER SCORE(ver graphics.js)*/
+      /* Llamando a la funcion de la GRAFICO NET PROMOTER SCORE(ver graphics.js, line:37)"aqp2016II"*/
       netPromoterScoreGraphic(20, 50, 17);
-      /* Llamando a la funcion de la GRAFICO TECH SKILLS (ver graphics.js)*/
-      techSkillsGraphic(techGoalAccomplished, techGoalNotMet);
-      /* Llamando a la funcion de la GRAFICO TECH SKILLS POR SPRINT(ver graphics.js)*/
+      /* Llamando a la funcion de la GRAFICO TECH SKILLS (ver graphics.js, line:56)"aqp2016II"*/
+      techSkillsGraphic(18, 30);
+      /* Llamando a la funcion de la GRAFICO TECH SKILLS por sprints(ver graphics.js, line:73)"aqp2016II"*/
       techSkillsSprintsGraphic(15, 5, 6, 10);
-      /* Llamando a la funcion de la GRAFICO LIFE SKILLS (ver graphics.js)*/
-
-
-      /* Llamando a la funcion de la GRAFICO STUDENTS SATISFACTION (ver graphics.js)*/
-      /* Llamando a la funcion de la GRAFICO TEACHER RATING(ver graphics.js)*/
-      /* Llamando a la funcion de la GRAFICO JEDI MASTER RATING (ver graphics.js)*/
+      /* Llamando a la funcion de la GRAFICO LIFE SKILLS (ver graphics.js, line:110)"aqp2016II"*/
+      lifesSkillsGraphic(14, 18);
+      /* Llamando a la funcion de la GRAFICO TECH SKILLS por sprints(ver graphics.js, line:127)"aqp2016II"*/
+      lifeSkillsSprintsGraphic(5, 6, 7, 8);
+      /* Llamando a la funcion de la GRAFICO STUDENTS SATISFACTION (ver graphics.js, line:163)"aqp2016II"*/
+      studentSatisfactionGraphic(3, 5, 7, 10);
+      /* Llamando a la funcion de la GRAFICO TEACHER RATING(ver graphics.js, line:182, line:201)"aqp2016II"*/
+      teacherRatingGraphic(35, 54, 23, 22);
+      /* Llamando a la funcion de la GRAFICO JEDI MASTER RATING (ver graphics.js)"aqp2016II"*/
+      jediMasterRatingGraphic(34, 12, 23, 34);
 
       /* 13 funciones*/
       /* Evento para tech y life por sprint*/
-      /* selectSprintTech.addEventListener('change', showSprintTech);
+      selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
         switch (true) {
         case event.target.value === 'sprint1':
+        /* Llamando a la funcion de la GRAFICO TECH SKILLS POR SPRINT(cada sprint individual)(ver graphics.js, line:92)"aqp2016II"*/
+          techSkillsSprintGraphic(7, 8);
+          break;
+        case event.target.value === 'sprint2':
+          techSkillsSprintGraphic(9, 8);
+          break;
+        case event.target.value === 'sprint3':
+          techSkillsSprintGraphic(7, 8);
+          break;
+        case event.target.value === 'sprint4':
+          techSkillsSprintGraphic(7, 8);
+          break;
+        default:
+        }
+      }
+      selectSprintLife.addEventListener('change', showSprintLife);
+      function showSprintLife(event) {
+        switch (true) {
+        case event.target.value === 'sprint1':
+        /* Llamando a la funcion de la GRAFICO TECH SKILLS POR SPRINT(cada sprint individual)(ver graphics.js, line:146)"aqp2016II"*/
+          lifeSkillsSprintGraphic(7, 8);
           break;
         case event.target.value === 'sprint2':
           break;
@@ -59,25 +84,19 @@ window.addEventListener('load', function(event) {
         default:
         }
       }
-      selectSprintLife.addEventListener('change', showSprintLife);
-      function showSprintLife(event) {
-        switch (true) {
-        case event.target.value === 'sprint1':
-          break;
-        case event.target.value === 'sprint2':
-          break;
-        case event.target.value === 'sprint3':
-          break;
-        case event.target.value === 'sprint4':
-          break;
-        default:
-        }
-      }*/
       break;
     case event.target.value === 'aqp2017I':
       section.classList.remove('display-none');
-      enrollmentGraphic(10, 124);
+      enrollmentGraphic(10, 23);
       achievementGraphic(15, 50);
+      netPromoterScoreGraphic(20, 50, 17);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(15, 5, 6);
+      lifesSkillsGraphic(14, 18);
+      lifeSkillsSprintsGraphic(5, 6, 7);
+      studentSatisfactionGraphic(3, 5, 7);
+      teacherRatingGraphic(35, 54, 23);
+      jediMasterRatingGraphic(34, 12, 23);
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -106,8 +125,16 @@ window.addEventListener('load', function(event) {
       break;
     case event.target.value === 'cdm2017I':
       section.classList.remove('display-none');
-      enrollmentGraphic(10, 124);
+      enrollmentGraphic(10, 84);
       achievementGraphic(15, 50);
+      netPromoterScoreGraphic(20, 50, 17);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(15, 5, 6);
+      lifesSkillsGraphic(14, 18);
+      lifeSkillsSprintsGraphic(5, 6, 7);
+      studentSatisfactionGraphic(3, 5, 7);
+      teacherRatingGraphic(35, 54, 23);
+      jediMasterRatingGraphic(34, 12, 23);
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -136,8 +163,16 @@ window.addEventListener('load', function(event) {
       break;
     case event.target.value === 'cdm2017II':
       section.classList.remove('display-none');
-      enrollmentGraphic(10, 124);
+      enrollmentGraphic(3, 24);
       achievementGraphic(15, 50);
+      netPromoterScoreGraphic(20, 50, 17);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(15, 5);
+      lifesSkillsGraphic(14, 18);
+      lifeSkillsSprintsGraphic(5, 6);
+      studentSatisfactionGraphic(3, 5);
+      teacherRatingGraphic(35, 54);
+      jediMasterRatingGraphic(34, 12);
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -162,7 +197,16 @@ window.addEventListener('load', function(event) {
       break;
     case event.target.value === 'lima2016II':
       section.classList.remove('display-none');
-      drawGraphic(10, 124);
+      enrollmentGraphic(7, 12);
+      achievementGraphic(15, 50);
+      netPromoterScoreGraphic(20, 50, 17);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(15, 5);
+      lifesSkillsGraphic(14, 18);
+      lifeSkillsSprintsGraphic(5, 6);
+      studentSatisfactionGraphic(3, 5);
+      teacherRatingGraphic(35, 54);
+      jediMasterRatingGraphic(34, 12);
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
         switch (true) {
@@ -186,7 +230,16 @@ window.addEventListener('load', function(event) {
       break;
     case event.target.value === 'lima2017I':
       section.classList.remove('display-none');
-      drawGraphic(10, 124);
+      enrollmentGraphic(10, 56);
+      achievementGraphic(15, 34);
+      netPromoterScoreGraphic(20, 50, 17);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(15, 5, 34, 34);
+      lifesSkillsGraphic(14, 18);
+      lifeSkillsSprintsGraphic(5, 6, 34, 45);
+      studentSatisfactionGraphic(3, 5, 32, 34);
+      teacherRatingGraphic(35, 54, 23, 34);
+      jediMasterRatingGraphic(34, 12, 45, 36);
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
         switch (true) {
@@ -218,7 +271,16 @@ window.addEventListener('load', function(event) {
       break;
     case event.target.value === 'lima2017II':
       section.classList.remove('display-none');
-      drawGraphic(10, 124);
+      enrollmentGraphic(10, 13);
+      achievementGraphic(15, 46);
+      netPromoterScoreGraphic(20, 50, 17);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(15, 5);
+      lifesSkillsGraphic(14, 18);
+      lifeSkillsSprintsGraphic(5, 6);
+      studentSatisfactionGraphic(3, 5);
+      teacherRatingGraphic(35, 54);
+      jediMasterRatingGraphic(34, 12);
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
         switch (true) {
@@ -242,7 +304,17 @@ window.addEventListener('load', function(event) {
       break;
     case event.target.value === 'scl2016II':
       section.classList.remove('display-none');
-      drawGraphic(10, 124);
+      enrollmentGraphic(10, 24);
+      achievementGraphic(15, 34);
+      netPromoterScoreGraphic(34, 50, 17);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(6, 5, 34, 34);
+      lifesSkillsGraphic(14, 3);
+      lifeSkillsSprintsGraphic(5, 6, 7, 45);
+      studentSatisfactionGraphic(3, 3, 32, 34);
+      teacherRatingGraphic(35, 54, 5, 34);
+      jediMasterRatingGraphic(34, 4, 45, 36);
+
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
         switch (true) {
@@ -274,7 +346,16 @@ window.addEventListener('load', function(event) {
       break;
     case event.target.value === 'scl2017I':
       section.classList.remove('display-none');
-      drawGraphic(10, 124);
+      enrollmentGraphic(10, 13);
+      achievementGraphic(15, 23);
+      netPromoterScoreGraphic(20, 50, 12);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(15, 5, 23);
+      lifesSkillsGraphic(14, 18);
+      lifeSkillsSprintsGraphic(5, 6, 23);
+      studentSatisfactionGraphic(3, 34, 32);
+      teacherRatingGraphic(35, 54, 56);
+      jediMasterRatingGraphic(34, 12, 32);
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
         switch (true) {
@@ -302,7 +383,16 @@ window.addEventListener('load', function(event) {
       break;
     case event.target.value === 'scl2017II':
       section.classList.remove('display-none');
-      drawGraphic(10, 124);
+      enrollmentGraphic(10, 39);
+      achievementGraphic(15, 56);
+      netPromoterScoreGraphic(20, 45, 17);
+      techSkillsGraphic(18, 30);
+      techSkillsSprintsGraphic(15, 34);
+      lifesSkillsGraphic(14, 23);
+      lifeSkillsSprintsGraphic(6, 6);
+      studentSatisfactionGraphic(3, 34);
+      teacherRatingGraphic(35, 34);
+      jediMasterRatingGraphic(34, 23);
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
         switch (true) {
--- js/graphics.js
+++ js/graphics.js
@@ -4,8 +4,8 @@ google.charts.load('current', {packages: ['corechart']});
 google.charts.setOnLoadCallback(enrollmentGraphic);
 function enrollmentGraphic(dropout, students) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     [' Deserting Students', dropout],
     ['Active Students', students]
@@ -21,8 +21,8 @@ function enrollmentGraphic(dropout, students) {
 google.charts.setOnLoadCallback(achievementGraphic);
 function achievementGraphic(goalAccomplished, goalNotMet) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Students that meet the target ', goalAccomplished],
     ['Students that not meet the target', goalNotMet]
@@ -38,8 +38,8 @@ function achievementGraphic(goalAccomplished, goalNotMet) {
 google.charts.setOnLoadCallback(netPromoterScoreGraphic);
 function netPromoterScoreGraphic(promoters, passive, detractors) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Promoters', promoters],
     ['Passive', passive],
@@ -57,8 +57,8 @@ function netPromoterScoreGraphic(promoters, passive, detractors) {
 google.charts.setOnLoadCallback(techSkillsGraphic);
 function techSkillsGraphic(techGoalAccomplished, techGoalNotMet) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Students that meet the technological goal', techGoalAccomplished],
     ['Students that not meet technological goal', techGoalNotMet]
@@ -70,12 +70,12 @@ function techSkillsGraphic(techGoalAccomplished, techGoalNotMet) {
   var graphic = new google.visualization.PieChart(document.getElementById('graphic-tech-skills'));
   graphic.draw(data, opciones);
 }
-/* Grafico especifico(por sprint): total de estudiantes que superan la meta de puntos tÉcnicos en promedio por sprint*/
+/* Grafico especifico(por sprint): total de estudiantes que superan la meta de puntos tÉcnicos en promedio por sprints*/
 google.charts.setOnLoadCallback(techSkillsSprintsGraphic);
 function techSkillsSprintsGraphic(techSprintOne, techSprintTwo, techSprintThree, techSprintFour) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Sprint 1', techSprintOne],
     ['Sprint 2', techSprintTwo],
@@ -86,7 +86,24 @@ function techSkillsSprintsGraphic(techSprintOne, techSprintTwo, techSprintThree,
   var opciones = {'width': 400,
     'height': 250,
   };
-  var graphic = new google.visualization.PieChart(document.getElementById('graphic-tech-skills-sprints'));
+  var graphic = new google.visualization.ColumnChart(document.getElementById('graphic-tech-skills-sprints'));
+  graphic.draw(data, opciones);
+}
+/* Grafico : total de estudiantes que superan la meta de puntos tÉcnicos en promedio por SPRINT*/
+google.charts.setOnLoadCallback(techSkillsSprintGraphic);
+function techSkillsSprintGraphic(techGoalAccomplishedSprint, techGoalNotMetSprint) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
+  data.addRows([
+    ['Students that meet the technological goal for sprint', techGoalAccomplishedSprint],
+    ['Students that not meet technological goal for sprint', techGoalNotMetSprint]
+  ]
+  );
+  var opciones = {'width': 300,
+    'height': 200,
+  };
+  var graphic = new google.visualization.PieChart(document.getElementById('graphic-tech-skills-sprint'));
   graphic.draw(data, opciones);
 }
 /* GRAFICO PARA LA CONTENEDOR DIV "LIFE SKILLS" */
@@ -94,8 +111,8 @@ function techSkillsSprintsGraphic(techSprintOne, techSprintTwo, techSprintThree,
 google.charts.setOnLoadCallback(lifesSkillsGraphic);
 function lifesSkillsGraphic(lifeGoalAccomplished, lifeGoalNotMet) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Students that meet the attitudinal goal', lifeGoalAccomplished],
     ['Students that not meet attitudinal goal', lifeGoalNotMet]
@@ -111,8 +128,8 @@ function lifesSkillsGraphic(lifeGoalAccomplished, lifeGoalNotMet) {
 google.charts.setOnLoadCallback(lifeSkillsSprintsGraphic);
 function lifeSkillsSprintsGraphic(lifeSprintOne, lifeSprintTwo, lifeSprintThree, lifeSprintFour) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Sprint 1', lifeSprintOne],
     ['Sprint 2', lifeSprintTwo],
@@ -123,15 +140,32 @@ function lifeSkillsSprintsGraphic(lifeSprintOne, lifeSprintTwo, lifeSprintThree,
   var opciones = {'width': 300,
     'height': 160,
   };
-  var graphic = new google.visualization.PieChart(document.getElementById('graphic-lifes-Skills-sprints'));
+  var graphic = new google.visualization.ColumnChart(document.getElementById('graphic-lifes-Skills-sprints'));
+  graphic.draw(data, opciones);
+}
+/* Grafico : total de estudiantes que superan la meta de puntos tÉcnicos en promedio por SPRINT*/
+google.charts.setOnLoadCallback(lifeSkillsSprintGraphic);
+function lifeSkillsSprintGraphic(lifeGoalAccomplishedSprint, lifeGoalNotMetSprint) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
+  data.addRows([
+    ['Students that meet the attitudinal goal for sprint', lifeGoalAccomplishedSprint],
+    ['Students that not meet attitudinal goal for sprint', lifeGoalNotMetSprint]
+  ]
+  );
+  var opciones = {'width': 300,
+    'height': 200,
+  };
+  var graphic = new google.visualization.PieChart(document.getElementById('graphic-life-skills-sprint'));
   graphic.draw(data, opciones);
 }
 /* GRAFICO PARA LA CONTENEDOR DIV "STUDENT SATISFACTION" */
 google.charts.setOnLoadCallback(studentSatisfactionGraphic);
 function studentSatisfactionGraphic(studentSatisfactionOne, studentSatisfactionTwo, studentSatisfactionThree, studentSatisfactionFour) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Sprint 1', studentSatisfactionOne],
     ['Sprint 2', studentSatisfactionTwo],
@@ -142,15 +176,15 @@ function studentSatisfactionGraphic(studentSatisfactionOne, studentSatisfactionT
   var opciones = {'width': 350,
     'height': 180,
   };
-  var graphic = new google.visualization.PieChart(document.getElementById('graphic-student-satisfaction'));
+  var graphic = new google.visualization.ComboChart(document.getElementById('graphic-student-satisfaction'));
   graphic.draw(data, opciones);
 }
 /* GRAFICO PARA LA CONTENEDOR DIV "TEACHER RATING" */
 google.charts.setOnLoadCallback(teacherRatingGraphic);
 function teacherRatingGraphic(teacherRatingOne, teacherRatingTwo, teacherRatingThree, teacherRatingFour) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Sprint 1', teacherRatingOne],
     ['Sprint 2', teacherRatingTwo],
@@ -161,15 +195,15 @@ function teacherRatingGraphic(teacherRatingOne, teacherRatingTwo, teacherRatingT
   var opciones = {'width': 350,
     'height': 180,
   };
-  var graphic = new google.visualization.PieChart(document.getElementById('graphic-teacher-rating'));
+  var graphic = new google.visualization.ComboChart(document.getElementById('graphic-teacher-rating'));
   graphic.draw(data, opciones);
 }
 /* GRAFICO PARA LA CONTENEDOR DIV "JEDI MASTER RATING" */
 google.charts.setOnLoadCallback(jediMasterRatingGraphic);
 function jediMasterRatingGraphic(jediMasterRatingOne, jediMasterRatingTwo, jediMasterRatingThree, jediMasterRatingFour) {
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'alumnas');
-  data.addColumn('number', 'count');
+  data.addColumn('string', 'students');
+  data.addColumn('number', 'quantity');
   data.addRows([
     ['Sprint 1', jediMasterRatingOne],
     ['Sprint 2', jediMasterRatingTwo],
@@ -180,6 +214,6 @@ function jediMasterRatingGraphic(jediMasterRatingOne, jediMasterRatingTwo, jediM
   var opciones = {'width': 350,
     'height': 180,
   };
-  var graphic = new google.visualization.PieChart(document.getElementById('graphic-jedi-master-rating'));
+  var graphic = new google.visualization.ComboChart(document.getElementById('graphic-jedi-master-rating'));
   graphic.draw(data, opciones);
 }
