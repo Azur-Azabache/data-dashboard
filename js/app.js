73b046acb8f30da7df938c0c47e0bb2c6ba7d81d
MariacristinaOrtiz <mari_d17@hotmail.com>
2017-11-30 05:42:33 -0500

    Añade graficos: logica inicial

--- js/app.js
+++ js/app.js
@@ -3,7 +3,26 @@ window.addEventListener('load', function(event) {
   console.log(event.target);/* recibe y ejecuta el evento me bota document*/
   console.log(event.currentTarget);/* escucha el click (tiene el addeventlistener por default)el evento me bota window*/
   /* escribir las funcione*/
-  var selectGeneration = document.getElementById('select-generation');
+  /* GRAFICOS*nuevo*/
+  google.charts.load('current', {packages: ['corechart']});
+  google.charts.setOnLoadCallback(drawGraphic);
+  function drawGraphic(dropout, students) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'alumnas');
+    data.addColumn('number', 'count');
+    data.addRows([
+      ['Alumn Dropout', dropout],
+      ['Total student', students]
+    ]
+    );
+    var opciones = {'width': 200,
+      'height': 120,
+    };
+    var graphic = new google.visualization.PieChart(document.getElementById('graphic-enrollment'));
+    graphic.draw(data, opciones);
+  }
+
+  var selectGeneration = document.getElementById('mySelect');
   var section = document.getElementById('section-display-none');
   /* accediendo a los divs vacios dodne agregaremos los valores de los indicadores*/
   var enrolled = document.getElementById('enrolled');
@@ -27,9 +46,14 @@ window.addEventListener('load', function(event) {
   /* Función a ejecutar*/
   function showGeneration(event) {
     switch (true) {
-    case event.target.value === 'aqp2016ii':
+    case event.target.value === 'aqp2016II':
       console.log(section);
       section.classList.remove('display-none');
+      /* Llamando a la funcion de la GRAFICO DOS*/
+      drawGraphic(130, 124);
+      /* Llamando a la funcion de la GRAFICO DOS*/
+      /* Llamando a la funcion de la GRAFICO DOS*/
+
       /* 13 funciones*/
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
@@ -61,7 +85,7 @@ window.addEventListener('load', function(event) {
         }
       }*/
       break;
-    case event.target.value === 'aqp2017ii':
+    case event.target.value === 'aqp2017I':
       section.classList.remove('display-none');
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
@@ -89,7 +113,7 @@ window.addEventListener('load', function(event) {
         }
       }*/
       break;
-    case event.target.value === 'cdm2017i':
+    case event.target.value === 'cdm2017I':
       selector.appendChild(optionThree);
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
@@ -117,7 +141,7 @@ window.addEventListener('load', function(event) {
         }
       }*/
       break;
-    case event.target.value === 'lim2017i':
+    case event.target.value === 'cdm2017II':
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -140,7 +164,7 @@ window.addEventListener('load', function(event) {
         }
       }*/
       break;
-    case event.target.value === 'lim2016ii':
+    case event.target.value === 'lima2016II':
       section.classList.remove('display-none');
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -163,61 +187,7 @@ window.addEventListener('load', function(event) {
         }
       }*/
       break;
-    case event.target.value === 'lim2017i':
-      section.classList.remove('display-none');
-      /* selectSprintTech.addEventListener('change', showSprintTech);
-      function showSprintTech(event) {
-        switch (true) {
-        case event.target.value === 'sprint1':
-          break;
-        case event.target.value === 'sprint2':
-          break;
-        case event.target.value === 'sprint3':
-          break;
-        case event.target.value === 'sprint4':
-          break;
-        default:
-        }
-      }
-      selectSprintLife.addEventListener('change', showSprintLife);
-      function showSprintLife(event) {
-        switch (true) {
-        case event.target.value === 'sprint1':
-          break;
-        case event.target.value === 'sprint2':
-          break;
-        case event.target.value === 'sprint3':
-          break;
-        case event.target.value === 'sprint4':
-          break;
-        default:
-        }
-      }*/
-      break;
-    case event.target.value === 'lim2017ii':
-      section.classList.remove('display-none');
-      /* selectSprintTech.addEventListener('change', showSprintTech);
-      function showSprintTech(event) {
-        switch (true) {
-        case event.target.value === 'sprint1':
-          break;
-        case event.target.value === 'sprint2':
-          break;
-        default:
-        }
-      }
-      selectSprintLife.addEventListener('change', showSprintLife);
-      function showSprintLife(event) {
-        switch (true) {
-        case event.target.value === 'sprint1':
-          break;
-        case event.target.value === 'sprint2':
-          break;
-        default:
-        }
-      }*/
-      break;
-    case event.target.value === 'scl2016ii':
+    case event.target.value === 'lima2017I':
       section.classList.remove('display-none');
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -248,7 +218,61 @@ window.addEventListener('load', function(event) {
         }
       }*/
       break;
-    case event.target.value === 'scl2017i':
+    case event.target.value === 'lima2017II':
+      section.classList.remove('display-none');
+      /* selectSprintTech.addEventListener('change', showSprintTech);
+      function showSprintTech(event) {
+        switch (true) {
+        case event.target.value === 'sprint1':
+          break;
+        case event.target.value === 'sprint2':
+          break;
+        default:
+        }
+      }
+      selectSprintLife.addEventListener('change', showSprintLife);
+      function showSprintLife(event) {
+        switch (true) {
+        case event.target.value === 'sprint1':
+          break;
+        case event.target.value === 'sprint2':
+          break;
+        default:
+        }
+      }*/
+      break;
+    case event.target.value === 'scl2016II':
+      section.classList.remove('display-none');
+      /* selectSprintTech.addEventListener('change', showSprintTech);
+      function showSprintTech(event) {
+        switch (true) {
+        case event.target.value === 'sprint1':
+          break;
+        case event.target.value === 'sprint2':
+          break;
+        case event.target.value === 'sprint3':
+          break;
+        case event.target.value === 'sprint4':
+          break;
+        default:
+        }
+      }
+      selectSprintLife.addEventListener('change', showSprintLife);
+      function showSprintLife(event) {
+        switch (true) {
+        case event.target.value === 'sprint1':
+          break;
+        case event.target.value === 'sprint2':
+          break;
+        case event.target.value === 'sprint3':
+          break;
+        case event.target.value === 'sprint4':
+          break;
+        default:
+        }
+      }*/
+      break;
+    case event.target.value === 'scl2017I':
       section.classList.remove('display-none');
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -275,7 +299,7 @@ window.addEventListener('load', function(event) {
         }
       }*/
       break;
-    case event.target.value === 'scl2017ii':
+    case event.target.value === 'scl2017II':
       section.classList.remove('display-none');
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -300,6 +324,7 @@ window.addEventListener('load', function(event) {
 
       break;
     default:
+      section.classList.add('display-none');
     }
   }
 });
---
+++ js/graphics.js
@@ -0,0 +1,17 @@
+/*google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawGraphic);
+function drawGraphic() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'alumnas');
+  data.addColumn('number', 'count');
+  data.addRows([
+    ['Alumn Desert', 139],
+    ['Alumn Active', 122]
+  ]
+  );
+  var opciones = {'width': 200,
+    'height': 120,
+  };
+  var graphic = new google.visualization.PieChart(document.getElementById('graphic-enrollment'));
+  graphic.draw(data, opciones);
+}*/
