e02ecca5cd78fffa546a79c97623d079e98a1e2d
Azur-Azabache <tharuka_20@hotmail.com>
2017-11-30 09:08:22 -0500

    Añadiendo funcion achievement

--- js/app.js
+++ js/app.js
@@ -25,8 +25,8 @@ window.addEventListener('load', function(event) {
   /* EVENTO CHANGE PARA EL FILTRO POR GENERACIONES*/
   selectGeneration.addEventListener('change', showGeneration);
   /* Función a ejecutar*/
-  // Funcion para llenar la cantidad de alumnos desertores
 
+  // Funcion para llenar la cantidad de alumnos desertores
   function showGeneration(event) {
     function countOff(obj) {
       var counter = 0;
@@ -35,8 +35,36 @@ window.addEventListener('load', function(event) {
           counter++;
         }
       }
+      return Math.round(100 * counter / arrStudent.length) + '%';
+    }
+    // Funcion para cantidad de alumnos que superan el 70% del puntaje total
+
+    function countProm(obj) {
+      var promedioMinimo = 2100;
+      var counter = 0;
+      var arrPromStudent = [];
+      var arrProm = [];
+      for (var i = 0; i < arrStudent.length; i++) {
+        if (arrStudent[i]['active'] === true) {
+          for (var x = 0; x < arrStudent[0]['sprints'].length; x++) {
+            var arr = Object.values(arrStudent[i]['sprints'][x]['score']);
+            var sum = (arr[0] + arr[1]);
+            arrProm.push(sum);
+          }
+        }
+      }
+      for (var z = 0; z < arrProm.length; z += 4) {
+        var studentSprint = (arrProm[z] + (arrProm[z + 1]) + (arrProm[z + 2]) + (arrProm[z + 3])) / 4;
+        arrPromStudent.push(studentSprint);
+      }
+      for (var y = 0; y < arrPromStudent.length; y++) {
+        if (arrPromStudent[y] > promedioMinimo) {
+          counter++;
+        }
+      }
       return counter;
     }
+    // Funcion para sacar el porcentaje del requisito anterior
 
     switch (true) {
     case event.target.value === 'lima2016II':
@@ -47,6 +75,8 @@ window.addEventListener('load', function(event) {
       enrolled.textContent = arrStudent.length;
       // Llenar la cantidad de alumnos desertores
       dropout.textContent = countOff(student);
+      achievement.textContent = countProm(student);
+      achievementPercent.textContent = (countProm(student) * 100) / arrStudent.length + '%';
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -84,6 +114,7 @@ window.addEventListener('load', function(event) {
       enrolled.textContent = arrStudent.length;
       // Llenar la cantidad de alumnos desertores
       dropout.textContent = countOff(student);
+      achievement.textContent = countProm(student);
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
@@ -117,6 +148,8 @@ window.addEventListener('load', function(event) {
       enrolled.textContent = arrStudent.length;
       // Llenar la cantidad de alumnos desertores
       dropout.textContent = countOff(student);
+      achievement.textContent = countProm(student);
+      achievementPercent.textContent = (countProm(student) * 100) / arrStudent.length + '%';
       /* Evento para tech y life por sprint*/
       /* selectSprintTech.addEventListener('change', showSprintTech);
       function showSprintTech(event) {
